4d8d27e5f9e0ef3b8e319de5e9a5ee41bf7f25f3
austin <austinsalguero08@gmail.com>
2023-04-05 11:15:03 -0500

    first commit

---
+++ src/Form/UserType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', emailType ::class , array('label' => 'Email'))
+            #->add('roles')
+            #->add('estado')
+            ->add('password', passwordType ::class , array('label' => 'Contraseña'))
+            ->add('ingresar', submitType ::class , array('label' => 'Ingresar'))
+            ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
